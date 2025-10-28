f8a65f849adaba6f99378f24f135aafa3362ceff
Victor Pavageau <victor.pavageau@gmail.com>
2025-10-28 19:00:51 +0100

    fix: contact form ignore captcha

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,12 +1,8 @@
-import type { FormInstance } from 'antd/es/form';
-import Form from 'antd/es/form';
 import type { JSX } from 'react';
 import { ContactForm, ContactHeader, ContactInfo } from '../components';
 import { useSubmitCooldown } from '../hooks';
 
 export default function ContactPage(): JSX.Element {
-  const [form]: [FormInstance] = Form.useForm();
-
   const {
     isInCooldown,
     remainingSeconds,
@@ -24,7 +20,6 @@ export default function ContactPage(): JSX.Element {
 
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
           <ContactForm
-            form={form}
             isInCooldown={isInCooldown}
             remainingSeconds={remainingSeconds}
             startCooldown={startCooldown}
